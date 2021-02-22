3fb12732028efa19f12d4b3a337d528fd315d47a
Antra Shrey <antrashrey@Antras-MacBook-Pro.local>
2021-02-22 11:01:32 +0530

    boiler plate

---
+++ src/test/java/com/example/springSession/SpringSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springSession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
